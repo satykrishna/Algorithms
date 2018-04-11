33f247905420e10823a839500b1be462b775f261
Satya <satykrishna@gmail.com>
2018-04-10 22:16:01 -0400

    Optionals are the better way to use, if you think there can be a chance of non presence of value

--- optional/java8/optional/client/OptionalApp.java
+++ optional/java8/optional/client/OptionalApp.java
@@ -1,6 +1,8 @@
 package java8.optional.client;
 
+import java.util.Map;
 import java.util.Optional;
+import java.util.Properties;
 
 import org.apache.log4j.Logger;
 
@@ -34,21 +36,71 @@ public class OptionalApp {
 	}
 
 	
-	private static void useOptionalsToAvoidNullChecks() {
-
-		Insurance insurance = new Insurance();
-
-		Optional<Insurance> optInsurance = Optional.ofNullable(insurance);
-
-		Optional<String> insuranceName = optInsurance.map(Insurance::getName);
-
+	private static Insurance findCheapInsurance(Person person, Car car) {
+		//Logic to find Cheap Insurance
+		Insurance cheapInsurance = new  Insurance();
+		return cheapInsurance;
 	}
 
+	public static Optional<Insurance> nullSafeFindingCheapInsurance(
+			Optional<Person> optionalPerson, Optional<Car> optionalCar) {
+		return optionalPerson.flatMap(person-> optionalCar.flatMap(car->Optional.ofNullable(findCheapInsurance(person, car))));
+	}
+	
+	private static void useOptionalsToAvoidNullChecks() {
+		Insurance insurance = new Insurance();
+		Optional<Insurance> optInsurance = Optional.ofNullable(insurance);
+		Optional<String> insuranceName = optInsurance.map(Insurance::getName);
+	}
+	
+	private static String getInsuranceforPersonOfAboveCertainAge(Person person, int minAge) {
+		return Optional.ofNullable(person)
+					   .filter(p -> p.getMinAge() > minAge)
+					   .flatMap(Person::getCarAsOptional)
+					   .flatMap(Car::getInsuranceAsOptional)
+					   .map(Insurance::getName)
+					   .orElse("UNKNOWN");
+	}
+
+	
+	public static Optional wrappingAPotentialNullValueinOptional(Map<String, String> map, String key) {
+	   return Optional.ofNullable(map.get(key));
+	}
+	
+	
+	public static Optional<Integer> stringToInt(String str) {
+		try {
+			return Optional.of(Integer.parseInt(str));
+		}
+		catch(NumberFormatException exception) {
+			return Optional.empty();
+		}
+	}
+	
+	public static int readDuration(Properties properties, String name ) {
+		return OptionalApp.stringToInt( properties.getProperty(name)).filter(i->i > 0).orElse(0);
+	}
+	
+	public static int readDurationOtherWay(Properties properties, String name ) {
+		 return  	Optional.ofNullable((String)properties.get(name))
+		  			.flatMap(OptionalApp::stringToInt)
+		   			.filter(i->i>0)
+		   			.orElse(0);
+	}
+	
+	
 	public static void main(String[] args) {
-
 		differentKindsOfOptionals();
-
 		useOptionalsToAvoidNullChecks();
+		usingOptionalsToFilter(null);
+	}
+
+	private static void usingOptionalsToFilter(Insurance insurance) {
+		
+		Optional.ofNullable(insurance)
+				.filter(i->"CAMBRIDGE".equals(i.getName()))
+				.ifPresent(System.out::println);
+		
 	}
 
 }
--- optional/java8/optional/example/Person.java
+++ optional/java8/optional/example/Person.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 public class Person {
 	
 	private Car car;
+	
+	private int minAge;
 
 	public Car getCar() {
 		return car;
@@ -16,6 +18,14 @@ public class Person {
 
 	public Optional<Car> getCarAsOptional() {
 		return Optional.ofNullable(car);
+	}
+
+	public int getMinAge() {
+		return minAge;
+	}
+
+	public void setMinAge(int minAge) {
+		this.minAge = minAge;
 	}
 	
 
